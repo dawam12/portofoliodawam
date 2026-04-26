bfe499c928cb37a800f359cf3432877431abae4a
dawam12 <mmdawam12amru@gmail.com>
2026-04-26 10:51:06 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Portfolio - IT Professional',
+  title: 'Portfolio - IT junior Developer',
   description: 'IT junior Portfolio - junior Developer',
   generator: 'v0.app',
   icons: {
